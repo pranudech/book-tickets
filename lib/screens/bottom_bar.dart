5767b088dfcfa8123fa3ac006c994418a5763e90
Pranudech <pranudech@gmail.com>
2023-03-29 17:02:00 +0700

    #update tickets

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:booktickets/screens/home_screen.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,7 @@ class BottomBar extends StatefulWidget {
 class _BottomBarState extends State<BottomBar> {
   int _selectedIndex = 0;
   static final List<Widget> _widgetOption = <Widget>[
+    HomeScreen(),
     const Text("Home"),
     const Text("Search"),
     const Text("Tickets"),
@@ -27,9 +29,9 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My tickets"),
-      ),
+      // appBar: AppBar(
+      //   title: const Text("My tickets"),
+      // ),
       body: Center(
         child: _widgetOption[_selectedIndex],
       ),
